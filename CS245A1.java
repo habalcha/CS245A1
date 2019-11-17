7ca47731a4de73bea34280017eb1414f9f1c3d0f
Hermon Balcha <47462628+habalcha@users.noreply.github.com>
2019-11-16 19:04:27 -0800

    Add files via upload

---
+++ CS245A1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Properties;
+public class CS245A1{
+
+    String link = "https://raw.githubusercontent.com/habalcha/jazzy/master/resource/dict/english.0";
+    URL english = new URL(link);
+
+    public void readDict(){
+        File file = new File();
+        Scanner sc = new Scanner(file);
+    }
+
+    public static void main(String[] args){
+
+    }
+}
+
